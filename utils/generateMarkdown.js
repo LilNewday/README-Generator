9deb99868e357400d7e2bf10ecf28399ab171843
LilNewday <lilnewday@gmail.com>
2023-03-31 00:41:43 -0400

    completed the license badge function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,41 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license === 'MIT') {
+    badge = '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)(https://opensource.org/licenses/MIT)';
+  }
+  if(license === 'BSD') {
+    badge = '![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)(https://opensource.org/licenses/BSD-3-Clause)';
+  }
+  if (license === 'GPL') {
+    badge = '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)(https://www.gnu.org/licenses/gpl-3.0)';
+  }
+  if (license === 'ISC') {
+    badge = '![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)(https://opensource.org/licenses/ISC)';
+  }
+  if (license === 'Apache') {
+    badge = '![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)(https://opensource.org/licenses/Apache-2.0)';
+  }
+  if (license === 'Unlicense') {
+    badge = '![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)(http://unlicense.org/)';
+  }
+  else if (license === '') {
+      badge = '';
+  } 
+  return badge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  A
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  B
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
